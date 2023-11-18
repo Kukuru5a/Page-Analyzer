bc065b0a11522ce57dfcf51912e4aaf7847b5548
kukuru5a <romanzag@icloud.com>
2023-11-18 15:18:09 +0600

    ffff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,11 +96,9 @@ tasks.test {
     }
 }
 tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
         xml.required = true
-        csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+
     }
 }
 tasks.jacocoTestCoverageVerification {
